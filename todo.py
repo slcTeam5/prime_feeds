d61e6f57c1d1e1e354dc0282f7ed83cbc9efdf7b
Philip Kwesiga <phil.kwesiga@gmail.com>
2017-08-18 13:52:22 +0300

    added function to delete post

--- todo.py
+++ todo.py
@@ -9,26 +9,7 @@ def get_posts(id=None):
 
 def get_post_by_id(id):
     return requests.get(_url('/posts/'+ str(id)))
-"""
-def describe_task(task_id):
-    return requestsget(_url('/tasks/{:d}/'.format(task_id)))
 
-def add_task(summary, description=""):
-    return requests.post(_url('/tasks/'), json={
-            'summary': summary,
-            'description': description,
-            })
-
-def task_done(task_id):
-    return requests.delete(_url('/tasks/{:d}/'.format(task_id)))
-
-def update_task(task_id, summary, description):
-    url = _url('/tasks/{:d}/'.format(task_id))
-    return requests.put(url, json={
-            'summary': summary,
-            'description': description,
-            }).
-"""
 def update_post(title, body, author, id):
     update ={"title": title, "body": body,
              "author": author, "id": id}
@@ -48,10 +29,25 @@ def create_post(title, body, author, id):
             "author": author, "id": id}
     talk = requests.post(_url('/posts/'), json=post)
 
+def delete_post(id):
+    response = requests.delete(_url('/posts/'+str(id)))
+    return response.status_code
+
+
 #print get_posts_data()
 
 #create_post("team 5 again", "and again", "phil", "82")
+#update_post("team 5 again", "and again and again", "phil", "82")
+#print get_posts_data()
+print "====get one==="
+print get_one_post(82)
+print "===delete==="
+print delete_post(82)
+print "====get one===="
+print get_one_post(82)
+create_post("team 5 again", "and again", "phil", "82")
 update_post("team 5 again", "and again and again", "phil", "82")
 print get_one_post(82)
+
 
 
