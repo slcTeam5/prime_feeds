ad4107b90bdb9067cfc8054ee1408cbb61561efb
gordon kisakye <gordon56692@gmail.com>
2017-08-18 14:06:30 +0300

    Improved Application

--- news_feed.py
+++ news_feed.py
@@ -4,16 +4,25 @@ def url(path):
     return 'http://34.207.10.230:3000' + path
 
 
-def view_feeds():
-	pass
-
-def post_feed():
-	pass
+def view_feed():
+    resp = requests.get(url('/posts'))
+    print(resp.content)
 
 
-def comment(comment_post='', p_id='', c_id=''):
-    return requests.post('http://34.207.10.230:3000/comments', data=[{
-        "body": comment_post,
-        "postId": p_id,
-        "id": c_id
-    }])
+def post_news():
+    title = input("Enter News Feed Title Between '': ")
+    body = input("Enter News Feed Body Between '': ")
+    return requests.post(url('/posts'), json={
+        'title': title,
+        'body': body
+    })
+
+
+def comment():
+    postId = input("Enter News Feed Post Id Between '': ")
+    body = input("Enter News Feed Comment Between '': ")
+    return requests.post(url('/comments'), json={
+        "body": body,
+        "postId": postId
+    })
+
