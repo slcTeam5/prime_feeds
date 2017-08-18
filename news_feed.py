878b63a47bacf74095eae5442de480ea720ef6df
EleisonC <30932534+EleisonC@users.noreply.github.com>
2017-08-18 01:49:11 -0700

    add functionality for view feeds

--- news_feed.py
+++ news_feed.py
@@ -1,7 +1,13 @@
 import requests
 
 def view_feeds():
-	pass
+	
+	return requests.get('http://34.207.10.230:3000/posts')
+
+def view_specific_fee(posts_id):
+	
+	return requests.get('/http://34.207.10.230:3000/posts/{:id}' .format(posts_id))
+	
 
 def post_feed():
 	pass
