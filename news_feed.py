87dc4a30019e385171d14fe1ba3475851d8dd76a
EleisonC <30932534+EleisonC@users.noreply.github.com>
2017-08-18 03:35:59 -0700

    update for view data

--- news_feed.py
+++ news_feed.py
@@ -1,9 +1,12 @@
 import requests
 
 def view_feeds():
-	print( requests.get('http://34.207.10.230:3000/posts').json())
+	return( requests.get('http://34.207.10.230:3000/posts').json())
 
-
+def view_specific_feed(posts_id):
+	"""post_id represents the index of specific data in the posts"""
+    	data = requests.get('http://34.207.10.230:3000/posts').json()
+    	return data[posts_id]
 	
 
 def post_feed():
